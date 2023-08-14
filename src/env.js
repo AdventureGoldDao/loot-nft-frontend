b40e40d973b2448cd3be389f0f093717ab3153f2
kingyoto <kingyoto@king.com>
2023-08-14 14:40:21 +0800

    staging config

--- src/env.js
+++ src/env.js
@@ -7,9 +7,13 @@ const development = {
 //   "API_URL": "https://freemint-api.lootchain.com/api"
 // }
 
+// const production = {
+//   "ENV": "production",
+//   "API_URL": "https://freemint-api.lootchain.com/api"
+// }
 const production = {
-  "ENV": "production",
-  "API_URL": "https://freemint-api.lootchain.com/api"
+  "ENV": "development",
+  "API_URL": "https://test.adventuregold.org/api"
 }
 
 const env = { development, production }[process.env.NODE_ENV || 'development']
